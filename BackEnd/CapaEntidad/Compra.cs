e7e0b85e929f165228a56ed4c08e3e9e68c7c357
Lucy Sagastegui <sagasteguivalientel@iestptrujillo.net>
2024-10-07 10:51:50 -0500

    agregar compra

---
+++ BackEnd/CapaEntidad/Compra.cs
@@ -0,0 +1,11 @@
+﻿namespace CapaEntidad
+{
+    public class Compra
+    {
+        public int nidcompra { get; set; }
+        public string nidproveedor { get; set; }
+        public DateTime dfechacompra { get; set; }
+        public string ntotal { get; set; }
+  
+    }
+}
--- BackEnd/WebApi/Program.cs
+++ BackEnd/WebApi/Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddSingleton(provider =>
 builder.Services.AddScoped<AlumnoRepository>();
 builder.Services.AddScoped<AlumnoDomain>();
 
+builder.Services.AddScoped<CompraRepository>();
+builder.Services.AddScoped<CompraDomain>();
+
 // Registrar los controladores
 builder.Services.AddControllers();
 
