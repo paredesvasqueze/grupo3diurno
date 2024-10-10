564834de6c42414c55c80eb33dc3685fb41a94e5
Richard <terronescahuanar@iestptrujillo.net>
2024-10-10 09:15:55 -0500

    agregando producto

---
+++ BackEnd/CapaEntidad/producto.cs
@@ -0,0 +1,13 @@
+﻿namespace CapaEntidad
+{
+    public class producto
+    {
+        public int nidproducto { get; set; }
+        public string cnombre { get; set; }
+        public string cdescripcion { get; set; }
+        public decimal npreciounitario { get; set; }
+        public int nstock { get; set; }
+        public int nidcategoria { get; set; }
+        public DateTime dfecharegistro { get; set; }
+    }
+}
