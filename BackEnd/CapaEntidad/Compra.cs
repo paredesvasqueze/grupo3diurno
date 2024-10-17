74e08e5e1cfc0dc4725c758df77401f386d7e0b6
Lucy Sagastegui <sagasteguivalientel@iestptrujillo.net>
2024-10-17 09:32:56 -0500

    ..

--- BackEnd/CapaEntidad/Compra.cs
+++ BackEnd/CapaEntidad/Compra.cs
@@ -3,9 +3,9 @@
     public class Compra
     {
         public int nidcompra { get; set; }
-        public string nidproveedor { get; set; }
+        public string? nidproveedor { get; set; }
         public DateTime dfechacompra { get; set; }
-        public string ntotal { get; set; }
+        public string? ntotal { get; set; }
   
     }
 }
